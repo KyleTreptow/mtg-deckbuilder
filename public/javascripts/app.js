7085c1580bc7db55e786a909336bd35b838721b7
Kyle Treptow <treptow@apollointeractive.com>
2019-01-18 10:46:27 -0800

    change jade to pug

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,8 +18,8 @@ const app = new Vue({
           function(response) {
             if (response.status !== 200) {
               console.log(
-                'Looks like there was a problem. Status Code: ' +
-                response.status
+                'Looks like there was a problem. Status Code: '
+                + response.status
               );
               return;
             }
